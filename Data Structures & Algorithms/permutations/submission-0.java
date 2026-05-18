2f6806a018999524aa5aa88951d2f0ec1f3c1ee5
Trinity <atulsharma21772@gmail.com>
2026-05-18 20:06:07 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+    // sort the array before processing such that duplicates became adjacent
+        Arrays.sort(nums);
+        // create a boolean array to mark the element whether used or not
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>());
+        return result;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> path) {
+
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            //duplicate pruning
+            if (i>0 && nums[i]==nums[i-1] && !used[i-1]) {
+                continue;
+            }
+
+            //if already used
+            if(used[i]) continue;
+
+            //choose
+            used[i]=true;
+            path.add(nums[i]);
+            //explore
+            backtrack(nums, used, path);
+            //undo
+            path.remove(path.size()-1);
+            used[i]=false;
+        }
+    }
+}
