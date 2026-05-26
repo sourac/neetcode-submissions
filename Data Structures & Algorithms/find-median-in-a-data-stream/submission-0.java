a1ef87eea5bda88ef313030d064c53c5fb287c21
Trinity <atulsharma21772@gmail.com>
2026-05-26 23:50:37 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.java
@@ -0,0 +1,25 @@
+class MedianFinder {
+
+PriorityQueue<Integer> maxHeap; 
+    PriorityQueue<Integer> minHeap; 
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if(minHeap.size()>maxHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(maxHeap.size()==minHeap.size()){
+            return (maxHeap.peek()+minHeap.peek())/2.0;
+        }else{
+            return maxHeap.peek();
+        }
+    }
+}
