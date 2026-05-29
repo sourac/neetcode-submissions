d1b9015aadf89198b9fa969d644f1adebe764858
Trinity <atulsharma21772@gmail.com>
2026-05-29 19:17:35 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+        while(left<right){
+            int width=right-left;
+            int minHeight=Math.min(height[left],height[right]);
+            maxArea=Math.max(maxArea,width*minHeight);
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
