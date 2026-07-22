cc9035d4c13ac7d486650774fea06fd70c506720
Trinity <atulsharma21772@gmail.com>
2026-07-22 23:51:56 +0530

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+                //this is a sliding window problem with the variable window
+        //technique is to expand -> violate -> shrink
+
+        int maxLen=0;
+        int left=0;
+        Set<Character> set=new HashSet<>();
+        for(int right=0;right<s.length();right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLen=Math.max(maxLen,right-left+1);
+        }
+        return maxLen;
+    }
+}
