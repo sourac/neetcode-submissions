cb5dd822fbee5b3e8db2fa8765a24d2472f02180
Trinity <atulsharma21772@gmail.com>
2026-07-29 21:38:11 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l=0;
+		 int r=s.length()-1;
+		 while(l<=r) {
+			 if (s.charAt(l)==s.charAt(r)) {
+				l++;
+				r--;
+			}else {
+				return isPalindrome(s,l,r-1) || isPalindrome(s,l+1,r);
+			}
+		 }
+		 return true;
+    }
+    
+    private static boolean isPalindrome(String s, int l, int r) {
+			while(l<=r) {
+				if (s.charAt(l)==s.charAt(r)) {
+					l++;
+					r--;
+				}
+				else return false;
+			}
+		return true;
+	}
+}
